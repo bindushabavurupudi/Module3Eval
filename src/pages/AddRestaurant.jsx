9c6122a0c36172297db693a1ad01b926ee078c70
Bindusha Bavurupudi <bindushabavurupudi2006@gmail.com>
2026-01-03 20:09:41 +0530

    added

--- src/pages/AddRestaurant.jsx
+++ src/pages/AddRestaurant.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { RestaurantContext } from "../context/RestaurantContext";
 import { useNavigate } from "react-router-dom";
 
@@ -12,9 +12,36 @@ function AddRestaurant(){
     parkingLot: "true",
     image: "https://coding-platform.s3.amazonaws.com/dev/lms/tickets/7524df6e-46fa-4506-8766-eca8da47c2f1/2izhqnTaNLdenHYF.jpeg"
   });
-
+ const [name, setName] = useState("");
+ const [address, setAddress] = useState("");
+ const [type, setType] = useState("");
+ const [parking, setParking] = useState("");
+  const handleChange = (e) =>{
+    const 
+  }
   return (
-    
+    <div>
+        <h1>Add Restaurant</h1>
+
+        <input type="text" placeholder="Name" onChange={{handleChange}} />
+        <input type="text" placeholder="address" onChange={handleChange} />
+        <select name="type" onChange={handleChange}>
+            <option>Rajasthani</option>
+            <option>Gujarati</option>
+            <option>Mughlai</option>
+            <option>Jain</option>
+            <option>Thai</option>
+            <option>North Indian</option>
+            <option>South Indian</option>
+        </select>
+
+       <select name="parking">
+        <option>True</option>
+        <option>False</option>
+       </select>
+
+       <button onClick={handleSubmit}>Add</button>
+    </div>
   )
 }
 
