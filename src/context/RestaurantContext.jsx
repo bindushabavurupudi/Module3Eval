875ad312c8c0c3a9de0799f35ea0eb3381225ce8
Bindusha Bavurupudi <bindushabavurupudi2006@gmail.com>
2026-01-03 19:37:09 +0530

    added

--- src/context/RestaurantContext.jsx
+++ src/context/RestaurantContext.jsx
@@ -1,5 +1,21 @@
-function RestaurantContext(){
+import { createContext, useContext, useEffect, useState } from "react";
+
+export const RestaurantContext = createContext();
+
+function RestaurantContextProvider({children}){
+    const [restaurants, setRestaurants] = useState([]);
+
+    useEffect(()=>{
+       const data = JSON.parse(localStorage.getItem("evalData")) || [];
+       setRestaurants(data);
+    }, []);
+
+    useEffect(()=>{
+      localStorage.setItem("evalData", JSON.stringify(restaurants));
+    }, [restaurants]);
+
+    const addRestaurant = 
 
 }
 
-export default RestaurantContext;
+export default RestaurantContextProvider;
