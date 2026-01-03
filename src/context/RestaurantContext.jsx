aaa3294dcdfd155dbe100a20ac14f591177227f5
Bindusha Bavurupudi <bindushabavurupudi2006@gmail.com>
2026-01-03 20:00:41 +0530

    added

--- src/context/RestaurantContext.jsx
+++ src/context/RestaurantContext.jsx
@@ -2,40 +2,39 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 export const RestaurantContext = createContext();
 
-function RestaurantContextProvider({children}){
+function RestaurantContextProvider({ children }) {
     const [restaurants, setRestaurants] = useState([]);
 
-    useEffect(()=>{
-       const data = JSON.parse(localStorage.getItem("evalData")) || [];
-       setRestaurants(data);
+    useEffect(() => {
+        const data = JSON.parse(localStorage.getItem("evalData")) || [];
+        setRestaurants(data);
     }, []);
 
-    useEffect(()=>{
-      localStorage.setItem("evalData", JSON.stringify(restaurants));
+    useEffect(() => {
+        localStorage.setItem("evalData", JSON.stringify(restaurants));
     }, [restaurants]);
 
-    const addRestaurant = (restaurant) =>{
-        const newRestaurant = {...restaurants, resTaurantID: Date.now()};
-        setRestaurants([...restaurants, newRestaurant]);
+    const addRestaurant = (restaurant) => {
+        setRestaurants([...restaurants, { ...restaurant, resTaurantID: Date.now() }]);
     };
 
-    const deleteRestaurant = (id) =>{
-        const updated = restaurants.filter((item)=>{
+    const deleteRestaurant = (id) => {
+        const updated = restaurants.filter((item) => {
             item.restaurantID != id
         });
         setRestaurants(updated);
     };
 
-    const updateRestaurant = (id, updatedData)=>{
-        const updated = restaurants.map((item)=> 
-        item.restaurantID === id ? {...item, ...updatedData} : item
+    const updateRestaurant = (id, updatedData) => {
+        const updated = restaurants.map((item) =>
+            item.restaurantID === id ? { ...item, ...updatedData } : item
         );
         setRestaurants(updated);
     };
     return (
 
-        <RestaurantContext.Provider value={{restaurants, addRestaurant, deleteRestaurant, updateRestaurant}}>
-{children}
+        <RestaurantContext.Provider value={{ restaurants, addRestaurant, deleteRestaurant, updateRestaurant }}>
+            {children}
         </RestaurantContext.Provider>
     );
 
--- src/pages/AddRestaurant.jsx
+++ src/pages/AddRestaurant.jsx
@@ -1,7 +1,22 @@
 import { useContext } from "react";
+import { RestaurantContext } from "../context/RestaurantContext";
+import { useNavigate } from "react-router-dom";
 
 function AddRestaurant(){
-const {addRestaurant} = useContext(RestaurauntContext);
+  const {addRestaurant} = useContext(RestaurantContext);
+  const navigate = useNavigate();
+  const [form, setForm] = useStae({
+    restaurantName: "",
+    address : "",
+    type: "Rajasthani",
+    parkingLot: "true",
+    image: "https://coding-platform.s3.amazonaws.com/dev/lms/tickets/7524df6e-46fa-4506-8766-eca8da47c2f1/2izhqnTaNLdenHYF.jpeg"
+  });
+
+  return (
+    
+  )
 }
+
 
 export default AddRestaurant;
