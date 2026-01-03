703122e95bdb4957529a353f1fcf18b3fe87ecb7
Bindusha Bavurupudi <bindushabavurupudi2006@gmail.com>
2026-01-03 19:44:29 +0530

    added

--- src/context/RestaurantContext.jsx
+++ src/context/RestaurantContext.jsx
@@ -14,8 +14,31 @@ function RestaurantContextProvider({children}){
       localStorage.setItem("evalData", JSON.stringify(restaurants));
     }, [restaurants]);
 
-    const addRestaurant = 
+    const addRestaurant = (restaurant) =>{
+        const newRestaurant = {...restaurants, resTaurantID: Date.now()};
+        setRestaurants([...restaurants, newRestaurant]);
+    };
 
-}
+    const deleteRestaurant = (id) =>{
+        const updated = restaurants.filter((item)=>{
+            item.restaurantID != id
+        });
+        setRestaurants(updated);
+    };
+
+    const updateRestaurant = (id, updatedData)=>{
+        const updated = restaurants.map((item)=> 
+        item.restaurantID === id ? {...item, ...updatedData} : item
+        );
+        setRestaurants(updated);
+    };
+    return (
+
+        <RestaurantContext.Provider value={{restaurants, addRestaurant, deleteRestaurant, updateRestaurant}}>
+{children}
+        </RestaurantContext.Provider>
+    );
+
+};
 
 export default RestaurantContextProvider;
