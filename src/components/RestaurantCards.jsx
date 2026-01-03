880778edf438b5de5de5bac4d4de1d5944621a53
Bindusha Bavurupudi <bindushabavurupudi2006@gmail.com>
2026-01-03 20:49:38 +0530

    added

--- src/components/RestaurantCards.jsx
+++ src/components/RestaurantCards.jsx
@@ -1,5 +1,22 @@
-function RestaurantCards(){
+import { useContext } from "react";
+import { RestaurantContext } from "../context/RestaurantContext";
 
+function RestaurantCards(){
+const {restaurants} = useContext(RestaurantContext);
+return(
+
+    <div>
+        {restaurants.map((r)=>(
+            <div key={r.restaurantID}>
+                <h3>{r.restaurantName}</h3>
+                <p>{r.address}</p>
+                <p>{r.type}</p>
+                <p>{r.parkingLot ? "Parking Available": "No Parking"}</p>
+                <img src={r.image} alt="image" />
+            </div>
+        ))}
+    </div>
+)
 }
 
 export default RestaurantCards;
